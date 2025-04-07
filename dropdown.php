477ece9f08bdd032db1bee50828da98b4a6cba85
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-04-07 10:33:52 +0200

    fonctionnement dropdown

--- dropdown.php
+++ dropdown.php
@@ -8,26 +8,21 @@
 </head>
 
 <body>
-    <nav>
-        <div class="navbar">
-            <div class="container nav-container">
-                <input class="checkbox" type="checkbox">
-                <div class="hamburger-lines">
+    <header>
+        <input class="checkbox" type="checkbox">
+
+        <div class="header">
+            <div class="navBar">
+                <a href="/"><img src="logo.png" class="logo" alt="logo"></a>
+                <div class="hamburger">
                     <span class="line line1"></span>
                     <span class="line line2"></span>
                     <span class="line line3"></span>
                 </div>
-                <img src="logo.png" class="logo" alt="logo">
-                <div class="menu-items">
-                    <ul>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">about</a></li>
-                        <li><a href="#">blogs</a></li>
-                        <li><a href="#">portfolio</a></li>
-                        <li><a href="#">contact</a></li>
-                    </ul>
-                </div>
             </div>
         </div>
-    </nav>
+
+        <div class="dropdown">
+        </div>
+    </header>
 </body>
