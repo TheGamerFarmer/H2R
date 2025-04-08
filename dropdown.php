d8997d8439e7404560e69d8bd1e826b6c3811058
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-04-08 10:42:49 +0200

    fin dropdown et fix autres fichiers

--- dropdown.php
+++ dropdown.php
@@ -1,28 +1,28 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <title>Dropdown</title>
-    <link rel="icon" type="image/x-icon" href="/logo.png">
-    <link rel="stylesheet" type="text/css" href="dropdown.css">
-</head>
+<header>
+    <input class="checkbox" type="checkbox">
 
-<body>
-    <header>
-        <input class="checkbox" type="checkbox">
-
-        <div class="header">
-            <div class="navBar">
-                <a href="/"><img src="logo.png" class="logo" alt="logo"></a>
-                <div class="hamburger">
-                    <span class="line line1"></span>
-                    <span class="line line2"></span>
-                    <span class="line line3"></span>
-                </div>
+    <div class="header">
+        <div class="navBar">
+            <a href="/"><img src="logo.png" class="logo" alt="logo"></a>
+            <div class="hamburger">
+                <span class="line line1"></span>
+                <span class="line line2"></span>
+                <span class="line line3"></span>
             </div>
         </div>
+    </div>
 
-        <div class="dropdown">
+    <div class="dropdown">
+        <div class="content">
+            <div class="links">
+                <a href="/">Home</a>
+                <a href="/custom">Custom</a>
+            </div>
+
+            <img src="image.png" alt="moto">
         </div>
-    </header>
-</body>
+        <div class="bottom">
+            <p>© copyright 2024 - All rights reserved</p>
+        </div>
+    </div>
+</header>
