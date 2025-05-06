6056bf9c64dd42824a85de60c216ce554aa66c30
Raphael MATHERET <raphael.matheret@etu.univ-grenoble-alpes.fr>
2025-05-06 17:55:52 +0200

    fin page accueil

--- accueil/accueil.php
+++ accueil/accueil.php
@@ -12,50 +12,72 @@
         <?php include '../headerEtFooter/header.php'; ?>
 
         <main>
-            <div class="firstScreen">
-                <h1>Custom H2R</h1>
+            <div id="firstScreen">
+                <div id="titre">
+                    <h1>Custom H2R</h1>
+                </div>
                 <img src="/image.png" alt="logo">
             </div>
-            <div id="weAre">
-                <h2>We Are</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
-                     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
-            </div>
-            <h2>Our Best Custom</h2>
-            <div id="OurBestCustom">
-                
-                <div>
-                    <img src="/accueil/moto.jpeg" alt="image par défault" >
-                    <img src="/accueil/moto.jpeg" alt="image par défault" >
-                </div>
-                
-            </div>
-            <h2>Start Your Custom</h2>
-            <div id="StartYourCustom">
-                <div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
-                    <button type="button">Commencer</button>
-                </div>
-                
-            </div>
-            <h2>Contact us</h2>
-            <div id="ContactUs">
-                
-                <img src="/image.png" alt="image par défault">
-                <div>
-                    <input id="text" type="text" name="text" placeholder="text">
-                    <input id="text" type="text" name="text" placeholder="text">
-                    <input id="text" type="text" name="text" placeholder="text">
-                    <button type="button">button</button>
-                    <button type="button">button</button>
-                </div>
-                
-            </div>
 
+            <div id="content">
+                <div id="weAre" class="contentBox">
+                    <h2 class="title">We Are</h2>
+
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.
+                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                </div>
+
+                <div id="ourBestCustoms" class="contentBox">
+                    <h2 class="title">Our Best Custom</h2>
+
+                    <div>
+                        <img src="/accueil/moto.jpeg" alt="default image" >
+                        <img src="/accueil/moto.jpeg" alt="default image" >
+                    </div>
+
+                </div>
+
+                <div id="startYourCustom">
+                    <h2 class="title">Start Your Custom</h2>
+
+                    <div>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus.</p>
+                        <a href="/custom"><button type="button">Start</button></a>
+                    </div>
+
+                </div>
+
+                <div id="contactUs" class="contentBox">
+                    <h2 class="title">Contact us</h2>
+
+                    <div id="contactUsContent">
+                        <div id="contactUsImage">
+                            <img src="/image.png" alt="default image">
+                        </div>
+                        <form method="post">
+                            <input class="textField" type="email" name="email" placeholder="email" required>
+                            <input class="textField" type="text" name="nom" placeholder="First name, Name" required>
+                            <textarea class="textField" name="message" placeholder="message" required></textarea>
+                            <input type="submit" value="Send">
+                        </form>
+
+                        <?php
+                            if (isset($_POST["message"])) {
+                                $message = "This message has being sent by the contact formular of the site H2R.\r\nThe sender of this message is: " . $_POST["nom"] . "\r\n\r\n";
+
+                                mail("raphaelmatheret@gmail.com",
+                                    "contact H2R",
+                                    $message . $_POST["message"],
+                                    "From:H2R@zron.fr\r\nReply-To:" . $_POST["email"]);
+                            }
+                        ?>
+                    </div>
+                </div>
+            </div>
         </main>
 
         <?php include '../headerEtFooter/footer.php'; ?>
