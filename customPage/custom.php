a453728b3f3da9913c9bc31e309d242dfec34676
KyllianBotrel <kyllian.botrel@etu.univ-grenoble-alpes.fr>
2025-05-08 14:47:53 +0200

    upgrade custom

--- customPage/custom.php
+++ customPage/custom.php
@@ -12,26 +12,31 @@
     <?php include '../headerEtFooter/header.php'; ?>
 
     <main>
-      <ul>
-        <li><h1>Wheels:</h1></li>
-        <li><button class="favorite styled"  type="button">base</button></li>
-        <li><button class="favorite styled"  type="button">Wheel1</button></li>
-        <li><button class="favorite styled"  type="button">Wheel2</button></li>
-        <li><button class="favorite styled"  type="button">Wheel3</button></li>
-      </ul>
-      <img src="/image.png" alt="exemple">
-      <ul>
-        <li><h1>Colors:</h1></li>
-        <li><button class="favorite styled" id="base" type="button">base</button></li>
-        <li><button class="favorite styled" id="red" type="button">red</button></li>
-        <li><button class="favorite styled" id="green" type="button">green</button></li>
-        <li><button class="favorite styled" id="blue" type="button">blue</button></li>
-        <li><button class="favorite styled" id="yellow" type="button">yellow</button></li>
-        <li><button class="favorite styled" id="black" type="button">black</button></li>
-      </ul>
-      <ul>
-        <li>
-        </li>
+        <img id="ex" src="/image.png" alt="exemple">
+      <ul id="liste">
+          <ul>
+              <li><h1>Exhaust pipe:</h1></li>
+              <li><button class="favorite styled"  type="button">base</button></li>
+              <li><button class="favorite styled"  type="button">Pipe1</button></li>
+              <li><button class="favorite styled"  type="button">Pipe2</button></li>
+              <li><button class="favorite styled"  type="button">Pipe3</button></li>
+          </ul>
+        <ul>
+            <li><h1>Color:</h1></li>
+            <li><button class="favorite styled" id="base" type="button">base</button></li>
+            <li><button class="favorite styled" id="red" type="button">red</button></li>
+            <li><button class="favorite styled" id="green" type="button">green</button></li>
+            <li><button class="favorite styled" id="blue" type="button">blue</button></li>
+            <li><button class="favorite styled" id="yellow" type="button">yellow</button></li>
+            <li><button class="favorite styled" id="black" type="button">black</button></li>
+        </ul>
+        <ul>
+            <li><h1>Wheel:</h1></li>
+            <li><button class="favorite styled"  type="button">base</button></li>
+            <li><button class="favorite styled"  type="button">Wheel1</button></li>
+            <li><button class="favorite styled"  type="button">Wheel2</button></li>
+            <li><button class="favorite styled"  type="button">Wheel3</button></li>
+        </ul>
       </ul>
     </main>
 
